2fc58125b988096f9bf9983e2f0b466c6fc45389
Livia Braz <revoltz.282@gmail.com>
2023-10-03 20:50:20 -0300

    +aula 01
    dia 03/10/23

---
+++ aula01/PrimeiroProjeto.java
@@ -0,0 +1,16 @@
+package aula01;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world :]");
+		// ctrl + F11 = rodar o projeto
+		// sysout (ctrl + espaço)
+		// ctrl + / = comentario
+		// ctrl + s = salva o projeto
+		// ctrl + espaço = pode selecionar e depois colocar entre sysout ou outras coisas
+		
+	}
+
+}
